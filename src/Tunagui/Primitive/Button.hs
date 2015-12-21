c31f9c0f824cd4115367d1efed1b94b5f9d119ee
masatoko <masatokodo@gmail.com>
2015-12-21 18:12:15 +0900

    Add dummy code about making and freeing Button

--- src/Tunagui/Primitive/Button.hs
+++ src/Tunagui/Primitive/Button.hs
@@ -1,1 +1,15 @@
 module Tunagui.Primitive.Button where
+
+data Button
+
+newButton :: IO Button
+newButton = do
+  putStrLn "Add code making new Button here."
+  -- * Make button considering some settings from arguments
+  -- * Allocate video resource
+  return Button
+
+freeButton :: Button -> IO ()
+freeButton button = do
+  putStrLn "Add code freeing Button here."
+  return ()
