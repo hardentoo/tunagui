83d293507f6eeb1e1198f251d147c90826d01290
masatoko <masatokodo@gmail.com>
2016-01-04 18:49:59 +0900

    Change button size

--- app/Main.hs
+++ app/Main.hs
@@ -51,8 +51,8 @@ testLabel =
     withWindow (WinConfig "main" True (V2 300 300)) tuna $ \win -> do
       (beh, push) <- liftIO . sync $ newBehavior (0 :: Integer)
       _ <- runTuna tuna $ runWin win $ do
-        (btnP,wBtnP) <- Button.mkButton (Button.defaultConfig {Button.bcText = Just " + "})
-        (btnM,wBtnM) <- Button.mkButton (Button.defaultConfig {Button.bcText = Just " - "})
+        (btnP,wBtnP) <- Button.mkButton $ btnSize (Button.defaultConfig {Button.bcText = Just " + "})
+        (btnM,wBtnM) <- Button.mkButton $ btnSize (Button.defaultConfig {Button.bcText = Just " - "})
         (btnC,wBtnC) <- Button.mkButton (Button.defaultConfig {Button.bcText = Just " CLEAR "})
         (_,wLbl) <- Label.mkLabelB Label.defaultConfig (T.pack . show <$> beh)
         testOverwriteTreeOP (Container DirV [wBtnP,wBtnM,wLbl,wBtnC])
@@ -66,6 +66,8 @@ testLabel =
       liftIO . forever $ do
         putStrLn "."
         threadDelay 1000000
+  where
+    btnSize conf = conf {Button.bcMinWidth = Just 40, Button.bcMinHeight = Just 40}
 
 -- test :: IO ()
 -- test =
