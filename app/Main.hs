e56753e2764937747ebf4d0b2d450d3426d23d96
masatoko <masatokodo@gmail.com>
2016-01-07 01:48:32 +0900

    Add selection window

--- app/Main.hs
+++ app/Main.hs
@@ -18,21 +18,36 @@ import qualified Tunagui.Widget.Label   as Label
 import           Tunagui.Operation
 
 main :: IO ()
-main = testLabel
+main = select
 
-testButton :: IO ()
-testButton =
-  GUI.withTunagui $ \tuna ->
-    withWindow (WinConfig "main" True (V2 300 300)) tuna $ \win -> do
-      runTuna tuna $ runWin win $ do
-        ws1 <- Container DirV <$> mapM mkBtn [1..(5::Int)]
-        ws2 <- Container DirH <$> mapM mkBtn [6..(10::Int)]
-        testOverwriteTreeOP $ Container DirV [ws1,ws2]
-        testRenderTree
-        --
-      forever $ do
-        putStrLn "."
-        threadDelay 1000000
+select :: IO ()
+select = GUI.withTunagui $ \tuna ->
+  withWindow (WinConfig "Select" False (V2 300 300)) tuna $ \win -> do
+    runTuna tuna $ runWin win $ do
+      (btnButton, wRunButton) <- Button.mkButton $ Button.defaultConfig {Button.bcText = Just "Button test"}
+      (btnLabel, wRunLabel) <- Button.mkButton $ Button.defaultConfig {Button.bcText = Just "Label test"}
+      --
+      testOverwriteTreeOP $ Container DirV [wRunButton, wRunLabel]
+      testRenderTree
+      --
+      liftIO $ btnButton `onClick` testButton tuna
+      liftIO $ btnLabel `onClick` testLabel tuna
+    forever $ do
+      putStrLn "."
+      threadDelay 1000000
+
+testButton :: GUI.Tunagui -> IO ()
+testButton tuna =
+  withWindow (WinConfig "main" True (V2 300 300)) tuna $ \win -> do
+    runTuna tuna $ runWin win $ do
+      ws1 <- Container DirV <$> mapM mkBtn [1..(5::Int)]
+      ws2 <- Container DirH <$> mapM mkBtn [6..(10::Int)]
+      testOverwriteTreeOP $ Container DirV [ws1,ws2]
+      testRenderTree
+      --
+    forever $ do
+      putStrLn "."
+      threadDelay 1000000
   where
     mkBtn i = do
       (btn,w) <- Button.mkButton $ Button.defaultConfig {Button.bcText = text}
@@ -42,60 +57,24 @@ testButton =
         text = Just $ T.pack $ show i
         work = putStrLn $ "click: " ++ show i
 
-testLabel :: IO ()
-testLabel =
-  GUI.withTunagui $ \tuna ->
-    withWindow (WinConfig "main" True (V2 300 300)) tuna $ \win -> do
-      (beh, push) <- liftIO . sync $ newBehavior (0 :: Integer)
-      _ <- runTuna tuna $ runWin win $ do
-        (btnP,wBtnP) <- Button.mkButton (Button.defaultConfig {Button.bcText = Just "+"})
-        (btnM,wBtnM) <- Button.mkButton (Button.defaultConfig {Button.bcText = Just "-"})
-        (btnC,wBtnC) <- Button.mkButton (Button.defaultConfig {Button.bcText = Just "CLEAR"})
-        (_,wLbl) <- Label.mkLabelB Label.defaultConfig (T.pack . show <$> beh)
-        let cBtn = Container DirH [wBtnP, wBtnM]
-        testOverwriteTreeOP (Container DirV [cBtn,wLbl,wBtnC])
-        testRenderTree
-        --
-        liftIO $ do
-          btnP `onClick` sync (push . (+ 1) =<< sample beh)
-          btnM `onClick` sync (push . (+ (-1)) =<< sample beh)
-          btnC `onClick` sync (push 0)
+testLabel :: GUI.Tunagui -> IO ()
+testLabel tuna =
+  withWindow (WinConfig "main" True (V2 300 300)) tuna $ \win -> do
+    (beh, push) <- liftIO . sync $ newBehavior (0 :: Integer)
+    _ <- runTuna tuna $ runWin win $ do
+      (btnP,wBtnP) <- Button.mkButton (Button.defaultConfig {Button.bcText = Just "+"})
+      (btnM,wBtnM) <- Button.mkButton (Button.defaultConfig {Button.bcText = Just "-"})
+      (btnC,wBtnC) <- Button.mkButton (Button.defaultConfig {Button.bcText = Just "CLEAR"})
+      (_,wLbl) <- Label.mkLabelB Label.defaultConfig (T.pack . show <$> beh)
+      let cBtn = Container DirH [wBtnP, wBtnM]
+      testOverwriteTreeOP (Container DirV [cBtn,wLbl,wBtnC])
+      testRenderTree
+      --
+      liftIO $ do
+        btnP `onClick` sync (push . (+ 1) =<< sample beh)
+        btnM `onClick` sync (push . (+ (-1)) =<< sample beh)
+        btnC `onClick` sync (push 0)
 
-      liftIO . forever $ do
-        putStrLn "."
-        threadDelay 1000000
-
--- test :: IO ()
--- test =
---   GUI.withTunagui $ \tuna ->
---     -- 1st window
---     withWindow (WinConfig "main" True (V2 600 400)) tuna $ \win1 -> do
---       (beh,push) <- liftIO (sync (newBehavior (0::Integer)))
---       _ <- runTuna tuna $ runWin win1 $ do
---         (btn1, w1B) <- Button.mkButton (Button.defaultConfig {Button.bcText = Just "button1"})
---         (_, w1L) <- Label.mkLabelT Label.defaultConfig "Label"
---         (_, w1L') <- Label.mkLabelB Label.defaultConfig (T.pack . show <$> beh)
---         testOverwriteTreeOP (Container DirV [w1B,w1L,w1L'])
---         testRenderTree
---         liftIO . sync $ listen (onClick btn1) $ \p -> putStrLn $ "click (1): " ++ show p
---       -- 2nd window
---       withWindow (WinConfig "sub" False (V2 200 200)) tuna $ \win2 -> do
---         _ <- runTuna tuna $ runWin win2 $ do
---           (btn2, w2) <- Button.mkButton (Button.defaultConfig {Button.bcText = Just "button2"})
---           testOverwriteTreeOP (Container DirV [w2])
---           testRenderTree
---           liftIO . sync $ listen (onClick btn2) $ \p -> putStrLn $ "click (2): " ++ show p
---         --
---         let loop = do
---               putStrLn "."
---               threadDelay 1000000
---               -- q <- sync $ sample quit
---               -- unless q loop
---
---               -- TEST count
---               sync $ do
---                 i <- sample beh
---                 push $ i + 1
---
---               loop
---         liftIO loop
+    liftIO . forever $ do
+      putStrLn "."
+      threadDelay 1000000
