2d6488d591eaab6a1b089d4f54ba389b12270906
masatoko <masatokodo@gmail.com>
2016-01-06 20:20:23 +0900

    Add DimConf data

--- src/Tunagui/Widget/Component/Conf.hs
+++ src/Tunagui/Widget/Component/Conf.hs
@@ -1,1 +1,11 @@
 module Tunagui.Widget.Component.Conf where
+
+data DimConf a = DimConf
+  {
+    maxv :: Maybe a
+  , minv :: Maybe a
+  , padding1 :: a
+  , padding2 :: a
+  , margin1 :: a
+  , margin2 :: a
+  } deriving Show
