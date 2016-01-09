2ad7c27a4091be836290ff2daed62d778bd8d355
masatoko <masatokodo@gmail.com>
2016-01-09 18:34:16 +0900

    Set blend mode of Texture Additive

--- src/Tunagui/Internal/Render.hs
+++ src/Tunagui/Internal/Render.hs
@@ -48,7 +48,9 @@ onTexture texture f = do
 createTexture :: V2 Int -> RenderT SDL.Texture
 createTexture size = do
   r <- ask
-  SDL.createTexture r SDL.RGBA8888 SDL.TextureAccessTarget (fromIntegral <$> size)
+  tex <- SDL.createTexture r SDL.RGBA8888 SDL.TextureAccessTarget (fromIntegral <$> size)
+  SDL.textureBlendMode tex $= SDL.BlendAdditive
+  return tex
 
 destroyTexture :: MonadIO m => SDL.Texture -> m ()
 destroyTexture = SDL.destroyTexture
